1dab36ad93a57d10db47f103424da026436ad377
Spring_MT <today.is.sky.blue.sky@gmail.com>
2013-11-11 10:56:50 +0900

    Update array_push_vs_arrows.rb

--- ruby/array_push_vs_arrows.rb
+++ ruby/array_push_vs_arrows.rb
@@ -9,4 +9,8 @@ Benchmark.bm(7, ">total:", ">ave:") do |x|
   b = x.report("<<") { n.times{ array << 1 } }
 end
 
-
+=begin
+% ruby array_push_vs_arrows.rb                                                            ~/bench/r              user     system      total        real
+push      0.160000   0.010000   0.170000 (  0.169633)
+<<        0.120000   0.000000   0.120000 (  0.126805)
+=end
