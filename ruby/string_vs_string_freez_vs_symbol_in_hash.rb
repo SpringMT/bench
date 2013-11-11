775778eae70cb1cfb3a4dbe0c5013582e44fd033
Spring_MT <today.is.sky.blue.sky@gmail.com>
2013-11-11 10:57:31 +0900

    Update string_vs_string_freez_vs_symbol_in_hash.rb

--- ruby/string_vs_string_freez_vs_symbol_in_hash.rb
+++ ruby/string_vs_string_freez_vs_symbol_in_hash.rb
@@ -10,3 +10,9 @@ Benchmark.bm(7, ">total:", ">ave:") do |x|
 end
 
 
+=begin
+% ruby string_vs_string_freez_vs_symbol_in_hash.rb                                        ~/bench/r              user     system      total        real
+string:   0.940000   0.020000   0.960000 (  0.971731)
+string_freez:  0.880000   0.030000   0.910000 (  0.919527)
+symbol:   0.740000   0.040000   0.780000 (  0.793753)
+=end
